f29da6b002989e2cfbd744ca968a307a020432eb
Taylor Salo <tsalo006@fiu.edu>
2019-10-31 19:13:48 -0400

    Add simple check.

--- math_task.py
+++ math_task.py
@@ -368,6 +368,8 @@ for curr_run in run_loop:
             rval_image.size = (0.3, 0.45)
             lval_image.pos = (-0.3, 0.0)
             rval_image.pos = (0.3, 0.0)
+        else:
+            raise Exception()
         op_image.setImage(r'numerals/{0}.png'.format(OPERATOR_DICT[operator]))
         comparison_image.setImage(r'numerals/{0:02d}n.png'.format(comparison))
         if solution > comparison:
